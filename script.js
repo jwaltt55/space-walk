5b4ef2b6e29db40dcd830ba9f747a5c61e64add8
justin walter <jwaltt55@gmail.com>
2021-04-25 17:59:36 -0400

    cleaned up code, added comments, hopefully fixed a bug

--- script.js
+++ script.js
@@ -1,64 +1,65 @@
+// a lot of globals :L
 let audioPlaying = false;
 let boxes = [];
 let analyser;
 let frequencies;
-
 const LIGHT_BLUE = new BABYLON.Color3(0.6, 0.851, 0.918);
-
 const NUM_MOVING_WORLD_STARS = 3000;
 const NUM_STATIC_WORLD_STARS = 12000;
-
 const NUM_STATIC_PATH_STARS = 13000;
 const NUM_MOVING_PATH_STARS = 2000;
-
 const PATH_LENGTH = 1200;
 const PATH_WIDTH = 6;
-
 const FFT_SIZE = 2048;
-
 let audioBuffer;
-
 let mushroomStemMaterial;
 let transparentMaterial;
-
 let NUM_CAP_STARS = 0;
-
+let NUM_STREAM_STARS = 5000;
 let capPositions = [];
 
+// once the window loads, do this
 window.onload = () => {
 
+    // get rid of the loading text
     document.getElementById('loadingtext').style.display = 'none';
     document.getElementById('loadingtext').style.zIndex = '-1';
     
+    // set up the canvas
     const canvas = document.getElementById('canvas');
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
-
     const engine = new BABYLON.Engine(canvas, true);
 
+    // set window resize behavior
     window.onresize = () => {
         canvas.height = window.innerHeight;
         canvas.width = window.innerWidth;
         engine.resize();
     }
 
+    // create the scene
     const scene = new BABYLON.Scene(engine);
     scene.clearColor = new BABYLON.Color3(0, 0, 0);
     scene.gravity = new BABYLON.Vector3(0, -0.15, 0);
+    scene.collisionsEnabled = true;
 
+    // create the camera, give it a hitbox and gravity
     const camera = new BABYLON.UniversalCamera("camera", new BABYLON.Vector3(5, 0, 0), scene);
     camera.setTarget(BABYLON.Vector3.Zero());
     camera.speed = 0.15;
     camera.attachControl(canvas, true);
     camera.applyGravity = true;
     camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
-    scene.collisionsEnabled = true;
     camera.checkCollisions = true;
+
+    // switch the camera controls to WASD
     camera.inputs.attached.keyboard.keysDown = [83];
     camera.inputs.attached.keyboard.keysUp = [87];
     camera.inputs.attached.keyboard.keysLeft = [65];
     camera.inputs.attached.keyboard.keysRight = [68];
 
+    // audio interface setup
     let audioContext = new AudioContext();
     let audioElement = document.getElementById('audio');
     let track = audioContext.createMediaElementSource(audioElement);
@@ -69,18 +70,16 @@ window.onload = () => {
     frequencies = new Uint8Array(analyser.frequencyBinCount);
     audioBuffer = new Float32Array(analyser.frequencyBinCount);
 
-    var material = new BABYLON.StandardMaterial(scene);
-    material.alpha = 1;
-    material.emissiveColor = new BABYLON.Color3(0.6, 0.851, 0.918);
-
+    // instantiate the two particle systems
     let staticParticles = new BABYLON.PointsCloudSystem('spcs', 1, scene);
     let dynamicParticles = new BABYLON.PointsCloudSystem('dpcs', 1, scene);
 
+    // instantiate the materials to use
     transparentMaterial = new BABYLON.StandardMaterial("", scene);
     transparentMaterial.alpha = 0;
-
     mushroomStemMaterial = new BABYLON.StandardMaterial('', scene);
 
+    // create the big world sphere thing
     let sphere = BABYLON.MeshBuilder.CreateSphere('', {diameter: 1200}, scene);
     sphere.position.infiniteDistance = true;
     sphere.material = transparentMaterial;
@@ -91,6 +90,7 @@ window.onload = () => {
         particle.velocity = new BABYLON.Vector3((Math.random() - .5) / 64, (Math.random() - .5) / 64, (Math.random() - .5) / 64);
     }
 
+    // create the path you walk on
     let path = BABYLON.MeshBuilder.CreateGround('', {width: PATH_LENGTH, height: PATH_WIDTH}, scene);
     path.position.y = -1;
     path.material = transparentMaterial;
@@ -102,6 +102,7 @@ window.onload = () => {
         particle.velocity = new BABYLON.Vector3((Math.random() - .5) / 256, 0, 0);
     }
     
+    // determine the positions of the mushrooms, make sure they're far enough away from each other
     for(let i = 0; i < Math.floor(Math.random() * 16) + 7; i++) {
         let capPosition = new BABYLON.Vector3(0, 0, 0);
         let notOverlapping = false;
@@ -121,28 +122,28 @@ window.onload = () => {
         }
     }
 
+    // spawn a random number of mushrooms
     let capWidth = Math.floor(80 * Math.random() + 100);
     let capHeight = capWidth - Math.floor(Math.random() * 60);
-    for(let i = 0; i < capPositions.length/*Math.floor(Math.random() * 9) + 5*/; i++) {
-        //let box = BABYLON.MeshBuilder.CreateBox("box", {size: Math.random() * 20 + 2}, scene);
-        
-        //box.position = new BABYLON.Vector3(Math.random() * 1600 - 8
+    for(let i = 0; i < capPositions.length; i++) {
         let cap = BABYLON.MeshBuilder.CreateSphere('', {diameterZ: capWidth, diameterX: capWidth, diameterY: capHeight}, scene);
-        //box.position = new BABYLON.Vector3(Math.random() * 1600 - 800, Math.random() * 1600 - 800, Math.random() * 1600 - 800);
-        cap.position = capPositions[i];//new BABYLON.Vector3(Math.floor((Math.random() - .5) * 1200 + 400), Math.floor((Math.random() - .5) * 1200), Math.floor((Math.random() - .5) * 1200));
+        cap.position = capPositions[i];
         cap.material = transparentMaterial;
-        let point1 = new BABYLON.Vector3(cap.position.x, cap.position.y + capHeight / 2/* - (Math.random() * 6 + 2)*/, cap.position.z);
+
+        // create the points for the stem
+        let point1 = new BABYLON.Vector3(cap.position.x, cap.position.y + capHeight / 2, cap.position.z);
         let point2 = new BABYLON.Vector3(point1.x + (Math.random() - .5) * 20, cap.position.y - (150 * Math.random() + 150), point1.z + (Math.random() - .5) * 20);
         let point3 = new BABYLON.Vector3(point2.x + (Math.random() - .5) * 20, point2.y - (150 * Math.random() + 150), point2.z + (Math.random() - .5) * 20);
         let point4 = new BABYLON.Vector3(point3.x + (Math.random() - .5) * 20, point3.y - (150 * Math.random() + 150), point3.z + (Math.random() - .5) * 20);
         let point5 = new BABYLON.Vector3(point4.x + (Math.random() - .5) * 20, point4.y - (150 * Math.random() + 150), point4.z + (Math.random() - .5) * 20);
         let point6 = new BABYLON.Vector3(point5.x + (Math.random() - .5) * 20, point5.y - (150 * Math.random() + 150), point5.z + (Math.random() - .5) * 20);
 
+        // create the main stem
         let rootRadius = (Math.random() * 3) + 1;
-
         let tube = BABYLON.MeshBuilder.CreateTube('', {path: [point1, point2, point3, point4, point5, point6], radius: rootRadius}, scene);
         tube.material = mushroomStemMaterial;
 
+        // give the stem some random roots
         let root1;
         let root2;
         let root3;
@@ -176,7 +177,6 @@ window.onload = () => {
                 radiusFunction: (i) => {if(i > 0) return 0; else return 1}
             }, scene);
         }
-       
         if(Math.random() < .5) {
             let root3x = point6.x + (Math.random() - .5) * 20;
             let root3y = point6.y - (150 * Math.random() + 150);
@@ -196,15 +196,14 @@ window.onload = () => {
                 radiusFunction: (i) => {if(i > 0) return 0; else return 1}
             }, scene);
         }
-        
         root1.material = mushroomStemMaterial;
         if(root2) root2.material = mushroomStemMaterial;
         root3.material = mushroomStemMaterial;
         if(root4) root4.material = mushroomStemMaterial;
 
+        // add all the particles for the stems/roots/caps
         let capParticles = 1800 + Math.floor(Math.random() * 1200);
         NUM_CAP_STARS += capParticles;
-        
         dynamicParticles.addSurfacePoints(cap, capParticles, BABYLON.PointColor.Stated, LIGHT_BLUE, 0);
         staticParticles.addVolumePoints(root1, 250 + Math.floor(Math.random() * 50), BABYLON.PointColor.Stated, LIGHT_BLUE, 0);
         if(root2)
@@ -215,6 +214,7 @@ window.onload = () => {
         staticParticles.addVolumePoints(tube, 3000 + Math.floor(Math.random() * 1000), BABYLON.PointColor.Stated, LIGHT_BLUE, 0);
     }
 
+    // save the original positions of the mushroom cap particles, so that they can respond to audio
     for(let i = NUM_MOVING_PATH_STARS + NUM_MOVING_WORLD_STARS; i < dynamicParticles.particles.length; i++) {
         let particle = dynamicParticles.particles[i];
             particle.originalPosition = new BABYLON.Vector3(
@@ -224,15 +224,40 @@ window.onload = () => {
             );
     }
 
+    // create the big square in the sky thing
+    let moon = BABYLON.MeshBuilder.CreateBox('', {height: 200, width: 200}, scene);
+    moon.material = transparentMaterial;
+    moon.position = new BABYLON.Vector3(1400, 1200, 1200);
+    staticParticles.addVolumePoints(moon, 10000, BABYLON.PointColor.Stated, LIGHT_BLUE, 0);
+
+    // create the streams that drip from the square
+    let stream1 = BABYLON.MeshBuilder.CreateTube('', {path: [new BABYLON.Vector3(1430, 1100, 1170), new BABYLON.Vector3(1430, -10000, 1170)], radius: 5}, scene);
+    stream1.material = transparentMaterial;
+    dynamicParticles.addVolumePoints(stream1, 6000, BABYLON.PointColor.Stated, LIGHT_BLUE, 0);
+    for(let i = dynamicParticles.particles.length - 6000; i < dynamicParticles.particles.length; i++) {
+        dynamicParticles.particles[i].originalPosition = new BABYLON.Vector3(dynamicParticles.particles[i].position.x, dynamicParticles.particles[i].position.y, dynamicParticles.particles[i].position.z);
+        dynamicParticles.particles[i].velocity = new BABYLON.Vector3(0, -0.5 * Math.random(), 0);
+    }
+    let stream2 = BABYLON.MeshBuilder.CreateTube('', {path: [new BABYLON.Vector3(1440, 1100, 1160), new BABYLON.Vector3(1440, -10000, 1160)], radius: 1}, scene);
+    stream2.material = transparentMaterial;
+    dynamicParticles.addVolumePoints(stream2, 4000, BABYLON.PointColor.Stated, LIGHT_BLUE, 0);
+    for(let i = dynamicParticles.particles.length - 4000; i < dynamicParticles.particles.length; i++) {
+        dynamicParticles.particles[i].originalPosition = new BABYLON.Vector3(dynamicParticles.particles[i].position.x, dynamicParticles.particles[i].position.y, dynamicParticles.particles[i].position.z);
+        dynamicParticles.particles[i].velocity = new BABYLON.Vector3(0, -0.5 * Math.random(), 0);
+    }
+
+    // build the particle meshes!
         staticParticles.buildMeshAsync().then(mesh => {
             dynamicParticles.buildMeshAsync().then(mesh => {
                 mesh.position.y -= 1;
-                engine.runRenderLoop(renderFunction);
+                    engine.runRenderLoop(renderFunction);
             });
         });
 
+    // this makes it so that you keep falling forever if you fall off the path
     camera._needMoveForGravity = true;
 
+    // this is what locks your pointer to the canvas, and what triggers the audio to play
     scene.onPointerDown = () => {
         canvas.requestPointerLock();
         if(!audioPlaying) {
@@ -240,9 +265,14 @@ window.onload = () => {
             audioPlaying = true;
         }
     }
-        
+    
+    // the render function! this is where the particles/materials get updated and redrawn
     function renderFunction() {
+
+        // get the audio data from the track for this frame
         analyser.getFloatTimeDomainData(audioBuffer);
+
+        // mushroom stem/root stuff
         if(mushroomStemMaterial.alpha <= 1)
             mushroomStemMaterial.alpha += .0125;
         let max = 0;
@@ -252,6 +282,9 @@ window.onload = () => {
         }
         if(1 - max < mushroomStemMaterial.alpha)
             mushroomStemMaterial.alpha = 1 - max;
+
+        // particle stuff! some particles have a velocity, some respond to the audio,
+        // and some need to get moved somewhere if the go out of bounds
         for(let i = 0; i < dynamicParticles.particles.length; i++) {
             let particle = dynamicParticles.particles[i];
             if(i < NUM_MOVING_WORLD_STARS) {
@@ -266,14 +299,33 @@ window.onload = () => {
                 else if(particle.position.x < PATH_LENGTH / -2)
                     particle.position.x = PATH_LENGTH;
             }
-            else {
+            else if(i < NUM_MOVING_WORLD_STARS + NUM_MOVING_PATH_STARS + NUM_CAP_STARS) {
                 particle.position.y = particle.originalPosition.y + audioBuffer[i % audioBuffer.length] * 25;
             }
+            else {
+                particle.position.z = particle.originalPosition.z + (audioBuffer[i % audioBuffer.length] * 1000) / (particle.position.y + 10000);
+                particle.position.x = particle.originalPosition.x + (audioBuffer[i % audioBuffer.length] * 1000) / (particle.position.y + 10000);
+                particle.position.y += particle.velocity.y;
+                if(particle.position.y < -10000)
+                    particle.position.y = 1100;
+                if(max > .94 && Math.random() > .99 && particle.position.y < -200)
+                    particle.position.y = 1100;
+            }
         }
-        dynamicParticles.setParticles();
-        
-        scene.render();
 
- 
+        // this is here because i think babylonjs has an undocumented bug related to the pointscloudsystem
+        // that will sometimes cause it to not add particles to something, which will cause an error when you
+        // try to redraw the particles :(
+        try {
+            dynamicParticles.setParticles();
+        } catch(e) {
+            document.getElementById('loadingtext').style.display = 'block';
+            document.getElementById('loadingtext').style.zIndex = '1';
+            document.getElementById('loadingtext').innerText = 'load failed, reloading...';
+            location.reload();
+        }
+        
+        // render the scene
+        scene.render();
     }
 }
